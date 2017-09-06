171cfeed982dc3ed98919f4000b4a7ce1a8360a3
Mark Helt and Sean Slingerland <mark@>
2017-09-06 10:56:04 -0700

    script version

--- lib/rock_paper_scissors.rb
+++ lib/rock_paper_scissors.rb
@@ -1,17 +1,26 @@
+#!/usr/bin/env ruby
+
 class Rps
   def initialize
   end
 
   def wins?(toss1, toss2)
     if ((toss1 == "rock") && (toss2 == "scissors"))
-      true
+      puts true
     elsif ((toss1 == "paper") && (toss2 == "rock"))
-      true
+      puts true
     elsif ((toss1 == "scissors") && (toss2== "paper"))
-      true
+      puts true
     else
-      false
+      puts false
     end
   end
 
 end
+puts "Rock, Paper, or Scissors"
+toss1 = gets.chomp
+puts "Rock, Paper, or Scissors"
+toss2 = gets.chomp
+
+game = Rps.new()
+game.wins?(toss1, toss2)
